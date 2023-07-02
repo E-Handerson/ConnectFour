3036eafcf01074a086f79e0848732b1ac5a660cf
Elizabeth Handerson <e.handerson578@mybvc.ca>
2023-07-02 02:02:21 -0600

    Teting Board class,variables and constructor

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConnectFour
         private static int _rows = 0;
         private static int _columns = 0;
 
-        public Board(int Rows, int Columns) 
+        public  Board(int Rows, int Columns) 
         {
             _rows = Rows;
             _columns = Columns;
