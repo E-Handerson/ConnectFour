f5916f917ddd99fcbc6a05381047484e0b8e6882
Elizabeth Handerson <e.handerson578@mybvc.ca>
2023-07-02 02:01:22 -0600

    Testing Board class and constructor

--- Program.cs
+++ Program.cs
@@ -2,7 +2,20 @@
 
 namespace ConnectFour
 {
-   
+    public class Board
+    {
+        private static int _rows = 0;
+        private static int _columns = 0;
+
+        public Board(int Rows, int Columns) 
+        {
+            _rows = Rows;
+            _columns = Columns;
+        }
+
+
+
+    }
 
     internal class Program
     {
